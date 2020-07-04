85cfcaa9c7d5a07d32fb450b5b9049465e6a85f1
Your Name <you@example.com>
2020-07-04 15:33:31 +0630

    Initialize Projcet

---
+++ src/des/DESImplementationMain.kt
@@ -0,0 +1,22 @@
+package des
+
+import com.desencrypt.textcryptor.DES
+
+fun main() {
+    val keys = "12345678"
+    val messages = "49206D69737320796F750D0A00000000"
+    println("Key     : $keys")
+    println("Message : $messages")
+
+    println()
+
+    val encoded = DES.encrypt(keys, messages)
+    println("Encoded : $encoded")
+
+    //println()
+
+   // val decoded = DES.decrypt(keys, encoded)
+   // println("Decoded : $decoded")
+
+
+}
